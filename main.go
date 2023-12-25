327b69f8852daf4d8456327a2e1fe3e0ad1b1806
kouakou kouamé yannick <yannickkouakou13@gmail.com>
2023-12-26 00:05:55 +0100

    feat: add function to create subfolders depending of the user choice between Javascript and typescript ✅

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"go-express-initializer/assets"
 	"go-express-initializer/utils"
 )
 
 func main() {
+	// VARIABLES
 	// ========== ASK FOR THE PROJECT NAME ========== //
 	var projectName string = fmt.Sprintf("%v", utils.FormatProjectName())
 	// ========== CREATE THE PROJECT FOLDER ========== //
 	utils.CreateFolder(projectName)
+	// ========== ASK FOR JAVASCRIPT🟨 OR TYPESCRIPT🟦 ========== //
+	var choice string = utils.AskJsorTs()
+	// ========== MAKE ACTIONS DEPENDING OF THE CHOICE ========== //
+	if choice == "js" {
+		utils.CreateJsSubFolders(projectName, assets.Folders_list_array)
+	} else if choice == "ts" {
+		utils.CreateTsSubFolders(projectName, assets.Folders_list_array)
+	}
 }
--- utils/utils.go
+++ utils/utils.go
@@ -10,9 +10,12 @@ import (
 
 // CREATE AND FORMAT THE PROJECT NAME
 func FormatProjectName() string {
-	fmt.Println("=========== EXPRESS JS INITIALIZER ==========")
+	fmt.Println("================ EXPRESS JS INITIALIZER ================")
+	fmt.Println("|----------- Developed by Yannick KOUAKOU ----------|")
+	fmt.Println()
 	fmt.Println("😏 What is your new Express Js 📁 project name ?: ")
 	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("your project name: ")
 	input, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println("an error has occurred while reading the name of your project:", err)
@@ -28,5 +31,55 @@ func CreateFolder(projectName string) {
 	err := os.Mkdir(projectName, os.ModePerm)
 	if err != nil && !os.IsExist(err) {
 		log.Fatal(fmt.Sprintf("Erreur lors de la création du dossier %s : %T - %v", projectName, err, err))
+	}
+	fmt.Println(" ")
+}
+
+// FUNCTION TO ASK BETWEEN JS & TS
+var choice int
+
+func AskJsorTs() string {
+	fmt.Println("What's your choice between JavaScript🟨 and TypeScript🟦: ")
+	fmt.Println("1 => JavaScript🟨 ")
+	fmt.Println("2 => TypeScript🟦")
+	fmt.Print("your choice: ")
+	fmt.Scan(&choice)
+	if int(choice) == 1 {
+		fmt.Println("you choose Javascript🟨")
+		fmt.Println(" ")
+		return "js"
+	} else {
+		fmt.Println("you choose TypeScript🟦")
+		fmt.Println(" ")
+		return "ts"
+	}
+}
+
+// CHOICE FOR TYPESCRIPT
+func CreateJsSubFolders(projectName string, subfolders []string) {
+	// create sub-folders in root
+	for _, sub := range subfolders {
+		sub = fmt.Sprintf("%s/%s", projectName, sub)
+		err := os.MkdirAll(sub, os.ModePerm)
+		if err != nil && !os.IsExist(err) {
+			log.Fatal(fmt.Sprintf("Erreur lors de la création du sous dossier %s : %T - %v", sub, err, err))
+		}
+	}
+}
+
+func CreateTsSubFolders(projectName string, subfolders []string) {
+	// create the source folder
+	const sourceDir string = "src"
+	err := os.MkdirAll(fmt.Sprintf("%s/%s", projectName, sourceDir), os.ModePerm)
+	if err != nil && !os.IsExist(err) {
+		log.Fatal(fmt.Sprintf("Erreur lors de la création du sous dossier source %s : %T - %v", sourceDir, err, err))
+	}
+	// create sub-folders in src
+	for _, sub := range subfolders {
+		sub = fmt.Sprintf("%s/%s/%s", projectName, sourceDir, sub)
+		err := os.MkdirAll(sub, os.ModePerm)
+		if err != nil && !os.IsExist(err) {
+			log.Fatal(fmt.Sprintf("Erreur lors de la création du sous dossier %s : %T - %v", sub, err, err))
+		}
 	}
 }
